c1710c7e02c738e76a2423541d3bf93574d2708f
Luis <luiswar03@gmail.com>
2024-12-10 08:45:34 +0100

    Beginning of Util class

---
+++ src/Util.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Util {
+public static String readString(Scanner sc, String s){
+    System.out.println(s);
+    String str = sc.nextLine();
+    return str;
+}
+
+}
